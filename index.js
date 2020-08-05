254f0b44a44e25c5ee5387d529ee166d2445fa32
Erix Mamani Villacresis <erixmv022@gmail.com>
2020-08-05 12:10:27 +0200

    midified routes

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
-app.use( bodyParser.json() ); 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 
 app.post("/api/cities", async (req, res) => {
-  res.json({id: process.env.REACT_APP_ALGOLIA_APP_ID, apikey: process.env.REACT_APP_ALGOLIA_API_KEY});
+  res.json({ id: process.env.REACT_APP_ALGOLIA_APP_ID, apikey: process.env.REACT_APP_ALGOLIA_API_KEY });
 });
 
-app.post('/api/forecast', async ({body}, res) => {
+app.post('/api/forecast', async ({ body }, res) => {
   try {
     const { data } = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${body.lat}&lon=${body.lng}&key=${process.env.REACT_APP_WEATHERBIT}&days=5`);
     res.json(data);
@@ -28,6 +25,9 @@ app.post('/api/forecast', async ({body}, res) => {
   }
 })
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
