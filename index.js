d5dd1250a25b128afec7272701f3648dcba8ecc9
Erix Mamani Villacresis <erixmv022@gmail.com>
2020-08-28 23:50:31 +0200

    added dotenv

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const axios = require("axios");
 const bodyParser = require("body-parser");
-const generatePassword = require('password-generator');
+
+//! Uncomment the next two lines for local running
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,8 +16,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // Put all API endpoints under '/api'
 app.post('/api/forecast', async ({ body }, res) => {
   try {
-    console.log(body);
-    const { data } = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${body.lat}&lon=${body.lng}&key=abc530c57199400ab07b04e58e04df1f&days=5`);
+    const { data } = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${body.lat}&lon=${body.lng}&key=${process.env.WEATHEREBIT_KEY}&days=5`);
     res.json(data);
   } catch (error) {
     console.log(error.message);
